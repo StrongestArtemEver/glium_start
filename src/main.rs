728aab84bc4121a6fa33edddee59307d9a753cbd
Fadin Artem <a.fadin-forwork@mail.ru>
2024-05-28 08:48:28 +0500

    add shaders

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,69 @@
 extern crate glium;
 
-use glium::{glutin,Surface};
+use glium::{glutin, implement_vertex, Surface};
 
 fn main() {
     let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
+    let wb = glutin::window::WindowBuilder::new()
+        .with_title("Треугольник")
+        .with_inner_size(glutin::dpi::LogicalSize::new(800.0, 600.0));
     let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb,cb,&event_loop).unwrap();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    // Определяем вершины треугольника
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex { position: [1.0, -0.5] };
+    let vertex2 = Vertex { position: [ 1.0,  0.5] };
+    let vertex3 = Vertex { position: [ -1.0, 0.0] };
+
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    // Создаем шейдеры
+    let vertex_shader_src = r#"
+        #version 140
+        in vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0); // Красный цвет
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
-            }
-            _ => (),
-        },
-        glutin::event::Event::NewEvents(cause) => match cause {
-            glutin::event::StartCause::Init => {
+                }
+                _ => (),
+            },
+            glutin::event::Event::RedrawEventsCleared => {
                 let mut target = display.draw();
-                target.clear_color(0.0, 0.0, 1.0, 1.0);
+                target.clear_color(0.0, 0.0, 1.0, 1.0); // Синий фон
+
+                // Отрисовываем треугольник
+                target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+
                 target.finish().unwrap();
             }
             _ => (),
-        },
-        _ => (),
         }
     });
 }
